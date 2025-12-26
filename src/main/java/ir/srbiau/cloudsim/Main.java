582bb95089fcac43c79ea90faa7dd011b0230996
mohammadRahmani317 <mohammadrahmani@gmail,com>
2025-12-26 23:52:39 +0330

    first commit

---
+++ src/main/java/ir/srbiau/cloudsim/Main.java
@@ -0,0 +1,12 @@
+
+package ir.srbiau.cloudsim;
+
+import org.cloudbus.cloudsim.Host;
+import org.cloudbus.cloudsim.examples.CloudSimExample1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CloudSimExample1.main(args);
+    }
+}
